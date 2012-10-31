ff5e9cf6b94d76de0a73909603440c4c079fe467
xiaojue <designsor@gmail.com>
2012-10-31 11:36:31 +0800

    修改了一下test并增加test文件，修改package.json

--- hfs.js
+++ hfs.js
@@ -8,7 +8,6 @@
 var fs = require("fs");
 var path = require("path");
 var $ = $ || {};
-console.log("已安装mass_hfs");
 var encodings = {
 	ascii: 1,
 	utf8: 1,
@@ -41,7 +40,6 @@ $.mix = function(receiver, supplier) {
 	return receiver;
 };
 $.mix({
-
 	noop: function() {},
 	createWriteStream: fs.createWriteStream,
 	createReadStream: fs.createReadStream,
@@ -415,56 +413,4 @@ $.mix({
 
 exports.$ = $;
 
-//2012.9.8 优化readFileSync
-
-//扼要测试代码
-//var cp = function(src, dst, callback) {//将一个文件的内容拷到另一个文件中，如果原来有东西，将被清掉再拷
-//    var is = fs.createReadStream(src); //因此这方法只用于文件间的内容转移
-//    var os = fs.createWriteStream(dst);//使用之前，要自己确保这两个文件是否存在
-//    require("util").pump(is, os, callback);
-//}
-//var a = 4
-//switch(a){
-//    case 1 ://create
-//        $.writeFile("files/45643/aara/test.js",'alert(88)', function(){
-//            $.writeFile("files/45643/aaa.js", "alert(1)",function(){
-//                console.log("创建文件与目录成功")
-//            })
-//        });
-//        $.mkdir("aaerewr",function(){
-//            console.log("创建目录成功")
-//        });
-//        break;
-//    case 2 ://walk
-//        $.walk("files",{
-//            cb:function(files,dirs){
-//                console.log(files);
-//                console.log(dirs)
-//                console.log("收集文件与目录，包括自身")
-//            },
-//            sync: true
-//        })
-//        break;
-//    case 3: //delete
-//        $.remove("files",function(files,dirs){
-//            console.log("删除文件与目录，包括自身")
-//        });
-//        $.remove("aaerewr",function(files,dirs){
-//            console.log("删除文件与目录，包括自身")
-//        });
-//        break;
-//    case 4://copy 
-//        console.log( path.resolve(process.cwd(), "E:\\aaa.js") +"!" )
-//        fs.writeFileSync("E:\\aaa.js","alert(1)");
-//        cp("helpers.js","neer/ee.txt",function(){
-//            console.log("ok")
-//        })
-//        $.cpdir("nbproject","E:\\neer");
-//        break;
-//}
-//
-//fs.rmdir("files/45643/aara/", function(){
-//    console.log("dd")
-//})
-//
 
